84c63a335ce4fe5bd0775727b812c584f54527c3
jane_medinatech <jane@medinatech.co>
2022-08-05 17:16:20 +0300

    Initial Commit

---
+++ SwiftBootcamp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftBootcamp
+//
+//  Created by Johny Alam on 5/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftBootcamp/SwiftBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBootcampApp.swift
+//  SwiftBootcamp
+//
+//  Created by Johny Alam on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
